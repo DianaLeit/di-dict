b6c8457ac3565de457c382d7bfd77c220861c40a
Leit <hanwi1994@gmail.com>
2024-01-03 16:24:45 +0100

    works with separate dictionary file

--- werkwoorden-checker.py
+++ werkwoorden-checker.py
@@ -1,9 +1,33 @@
 import random
 
-dictionary = {
-    "read": {"infinitief": "lezen", "verleden": "las", "voltooid": "gelezen"},
-    "see": {"infinitief": "zien", "verleden": "zag", "voltooid": "gezien"},
-}
+# dictionary = {
+#     "read": {"infinitief": "lezen", "verleden": "las", "voltooid": "gelezen"},
+#     "see": {"infinitief": "zien", "verleden": "zag", "voltooid": "gezien"},
+# }
+
+
+def make_dict() -> dict[str, dict[str, str]]:
+    file = open("werkwoorden-lijst.txt")
+    dict_strings = file.readlines()
+    dictionary = {}
+    for line in dict_strings:
+        parts = line.split("\t")
+        if parts[0] == "\n":
+            break
+        key = parts[3].strip()
+        inf = parts[0].strip()
+        imp = parts[1].split(",")[-1].strip()
+        perf = parts[2].strip()
+        dictionary[key] = {
+            "infinitief": f"{inf}",
+            "verleden": f"{imp}",
+            "voltooid": f"{perf}",
+        }
+    file.close()
+    return dictionary
+
+
+dictionary = make_dict()
 list_of_keys = list(dictionary.keys())
 random_translation = random.choice(list_of_keys)
 exp_inf = dictionary[random_translation]["infinitief"]
@@ -34,7 +58,10 @@ def check(given_inf: str, given_pas: str, given_perf: str, attempt: int) -> None
 
 
 answer = input(
-    f"Type verb in infinitief, verleden and voltooid time separated by space for '{random_translation}': "
+    f"Type verb in infinitief, imperfectum and perfectum time separated by space for '{random_translation}': "
 )
 list_of_answers = answer.split()
 check(list_of_answers[0], list_of_answers[1], list_of_answers[2], 3)
+
+# You are the best. I'm very proud of you and you can solve any problem.
+# Eventually. Believe in yourself! ❤️
